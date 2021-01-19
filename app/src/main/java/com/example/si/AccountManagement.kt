25afe9ac0cc6ab3a047d7b53e7ed687dae48da48
Vlad Ilie <ilievlad73@gmail.com>
2021-01-19 19:17:14 +0200

    Add example file upload.

--- app/src/main/java/com/example/si/AccountManagement.kt
+++ app/src/main/java/com/example/si/AccountManagement.kt
@@ -1,7 +1,10 @@
 package com.example.si
 
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +13,17 @@ import com.example.si.`object`.SavedPreferences
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_authentication.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_finish_account_configuration.*
+import java.io.File
+
 
 class AccountManagement : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseFirestore: FirebaseFirestore;
+    private lateinit var firebaseStorageReference: StorageReference;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,9 @@ class AccountManagement : AppCompatActivity() {
         // Init firebase firestore
         firebaseFirestore = FirebaseFirestore.getInstance()
 
+        // Init storage
+        firebaseStorageReference = FirebaseStorage.getInstance().reference
+
         // check if previous settings have been made
         first_name_edit_text.setText(SavedPreferences.getFirstName(this))
         last_name_edit_text.setText(SavedPreferences.getLastName(this))
@@ -37,11 +47,14 @@ class AccountManagement : AppCompatActivity() {
 
         // select file button
         select_files_button.setOnClickListener { _: View? ->
-            val getIntent = Intent()
-            getIntent.type = "image/*";
-            getIntent.action = Intent.ACTION_GET_CONTENT;
-            val chooserIntent = Intent.createChooser(getIntent, "Select file")
-            startActivityForResult(chooserIntent, Configs.SELECT_FILES_SUCCESS_REQUEST_CODE);
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+
+            startActivityForResult(
+                Intent.createChooser(intent, "Select a file"),
+                Configs.SELECT_FILES_SUCCESS_REQUEST_CODE
+            )
         }
 
         // sign_up_button
@@ -79,6 +92,56 @@ class AccountManagement : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode === Configs.SELECT_FILES_SUCCESS_REQUEST_CODE) {
             Log.d(this.localClassName, "Select files activity returned successfully.")
+
+            if (data != null && data.data != null) {
+                val uri: Uri? = data.data
+                if (uri != null) {
+                    Log.d(this.localClassName, uri.toString())
+//                    https://stackoverflow.com/questions/36329379/file-not-found-exception-path-from-uri
+//                    https://stackoverflow.com/questions/20067508/get-real-path-from-uri-android-kitkat-new-storage-access-framework/20413475#20413475
+//                    https://stackoverflow.com/questions/59123162/android-kotlin-getting-a-filenotfoundexception-with-filename-chosen-from-file-p
+
+                    val stream = contentResolver.openInputStream(uri)
+                    if (stream != null) {
+                        Log.d(this.localClassName, "File found ${getFileName(uri)}")
+
+                        val ref = firebaseStorageReference.child("test")
+
+                        val uploadTask = ref.putStream(stream)
+                        uploadTask.addOnFailureListener {
+                            // Handle unsuccessful uploads
+                            Log.d(this.localClassName, "File upload failed.")
+                        }.addOnSuccessListener { taskSnapshot ->
+                            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                            Log.d(this.localClassName, "File uploaded successfully.")
+                        }
+                    } else {
+                        Log.d(this.localClassName, "File not found")
+                    }
+                }
+            }
         }
+    }
+
+    private fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor?.close()
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
     }
 }
--- app/src/main/java/com/example/si/Home.kt
+++ app/src/main/java/com/example/si/Home.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.example.si.`object`.Configs
 import com.example.si.`object`.SavedPreferences
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_authentication.*
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +35,23 @@ class Home : AppCompatActivity() {
             finish()
         }
 
+        // SETTINGS BUTTON
+        setting_button.setOnClickListener {
+            startActivityForResult(
+                Intent(this, AccountManagement::class.java),
+                Configs.ACCOUNT_UPDATE_SUCCESS_REQUEST_CODE
+            )
+        }
+
         // Welcome text view
         welcome_text_view.text = "Welcome ${SavedPreferences.getEmail(this)}"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode === Configs.ACCOUNT_UPDATE_SUCCESS_REQUEST_CODE) {
+            Log.d(this.localClassName, "Account management activity returned successfully.")
+            // this will start anyway due to on start behaviour
+        }
     }
 }
